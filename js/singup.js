d774ce2722069e452e156aaf9eebb04c4c40150d
rocha1202 <40220247@esmad.ipp.pt>
2024-04-08 15:37:56 +0100

    calendario em registo

--- js/singup.js
+++ js/singup.js
@@ -1,7 +1,15 @@
-document.getElementById('dataNascimento').addEventListener('input', function (e) {
-    var value = e.target.value;
-    if (value.length === 2 || value.length === 5) {
-        value += '/';
-    }
-    e.target.value = value;
+$(function() {
+    var maxDate = new Date(); // Obtém a data atual
+    maxDate.setFullYear(maxDate.getFullYear() - 10); // Subtrai 10 anos da data atual
+
+    $("#dataNascimento").datepicker({
+        dateFormat: 'dd/mm/yy',
+        changeMonth: true,
+        changeYear: true,
+        yearRange: "-100:+0",
+        maxDate: maxDate, // Define a data máxima selecionável como 10 anos atrás
+        onSelect: function(dateText) {
+            $(this).val(dateText);
+        }
+    });
 });
