44c385b6a5c9a9af53977fe6776ae77c7ac7e3ca
Leonor Rodrigues <72989643+mleonor5@users.noreply.github.com>
2024-04-11 12:35:59 +0100

    pagina inicial update e pagina signup correcao

---
+++ js/sideBar.js
@@ -0,0 +1,9 @@
+function openNav() {
+  document.getElementById("sideBar").style.width = "250px";
+  document.getElementById("menu").style.opacity = 0;
+}
+
+function closeNav() {
+  document.getElementById("sideBar").style.width = 0;
+  document.getElementById("menu").style.opacity = "100%";
+}
