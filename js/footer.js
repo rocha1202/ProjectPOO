e00f80d6f47c9d3d21421ab8eeb8fc3ac532ad9d
Leonor Rodrigues <72989643+mleonor5@users.noreply.github.com>
2024-05-08 11:22:04 +0100

    sideBar e footer js

---
+++ js/footer.js
@@ -0,0 +1,52 @@
+const footer = document.querySelector(".footer");
+const footerDetail = document.querySelector(".footerDetail");
+
+const renderFooter = () => {
+  footer.innerHTML = `
+    <div class="d-flex justify-content-between footer_container">
+    <div class="logos col-3">
+      <img src=${
+        !footerDetail ? "./img/Logotipo.svg" : "../img/Logotipo.svg"
+      } alt="Logo TSIW" class="logo_footer"
+      />
+      <br />
+      <img
+          src=${
+            !footerDetail ? "./img/PPORTO_Cor.svg" : "../img/PPORTO_Cor.svg"
+          }
+          alt="Logo TSIW"
+          class="logo_footer"
+      />
+    </div>
+    <div class="col-3">
+      <p class="info_uni">
+        Escola Superior de Media Artes e Design <br />Rua D. Sancho I, n.º
+        981
+        <br />
+        4480-876 Vila do Conde <br />Portugal <br />
+        <br />
+        T. +351 252 291 700 <br />E. geral@esmad.ipp.pt
+      </p>
+    </div>
+    <div class="col-3 link_sociais">
+      <p>Vê as nossas redes sociais!</p>
+      <div class="d-flex icons">
+        <a href="https://www.instagram.com/_tsiw.esmad_/">
+          <i class="ph ph-instagram-logo"></i>
+        </a>
+        <a href="https://www.facebook.com/LTSIW">
+          <i class="ph ph-facebook-logo"></i>
+        </a>
+        <a href="https://www.youtube.com/channel/UCA1uYDftSXjN1Lx5E9-ByIg">
+          <i class="ph ph-youtube-logo"></i>
+        </a>
+        <a href="">
+          <i class="ph ph-envelope"></i>
+        </a>
+      </div>
+    </div>
+  </div>
+    `;
+};
+
+renderFooter();
--- js/sideBar.js
+++ js/sideBar.js
@@ -1,3 +1,36 @@
+const sideBar = document.querySelector(".sideBar");
+const sideBarDetail = document.querySelector(".sideBarDetail");
+
+const renderSideBar = () => {
+  sideBar.innerHTML += `<a onclick="closeNav()"><i class="ph ph-x closeBtn"></i></a>
+  <div class="nav_options d-flex flex-column">
+  <a href=${
+    !sideBarDetail
+      ? "#saidas_profissionais"
+      : "../index.html#saidas_profissionais"
+  } onclick="closeNav()"
+    >Saídas Profissionais</a
+  >
+  <a href=${
+    !sideBarDetail ? "#eventos_noticias" : "../index.html#eventos_noticias"
+  } onclick="closeNav()">Eventos/Notícias</a>
+  <a href="">Galeria</a>
+  <a href=${
+    !sideBarDetail ? "#sobre" : "../index.html#sobre"
+  } onclick="closeNav()">Sobre</a>
+  <a href=${
+    !sideBarDetail ? "#testemunhos" : "../index.html#testemunhos"
+  } onclick="closeNav()">Testemunhos</a>
+  <a href=${
+    !sideBarDetail ? "#contactos" : "../index.html#contactos"
+  } onclick="closeNav()">Contactos</a>
+  <a href=${!sideBarDetail ? "./html/login.html" : "./login.html"}>Login</a>
+</div>
+<div class="d-flex linguas"><a href="">EN</a> | <a href="">PT</a></div>`;
+};
+
+renderSideBar();
+
 function openNav() {
   document.getElementById("sideBar").style.width = "250px";
   document.getElementById("menu").style.opacity = 0;
