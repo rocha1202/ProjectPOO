1c1aac36ce226f5a31c12d9045876fb76ed99277
Leonor Rodrigues <72989643+mleonor5@users.noreply.github.com>
2024-06-08 12:38:02 +0100

    testemunho detalhe - vista admin

--- js/views/admin/Testemunhos/TestemunhosVerView.js
+++ js/views/admin/Testemunhos/TestemunhosVerView.js
@@ -9,6 +9,18 @@ function testemunhoView() {
   document.querySelector("#subTitulo").value = testemunho.sub_titulo;
   document.querySelector("#descricao").value = testemunho.descricao;
   document.querySelector("#file").value = testemunho.img;
+
+  renderButtons(testemunho.eliminado);
+}
+
+function renderButtons(eliminado) {
+  const btnList = document.querySelector("#btnList");
+  btnList.innerHTML = `
+  <button class="btn btn-outline-primary">Atualizar</button>
+  <button class="btn btn-outline-primary">${
+    eliminado === "S" ? "Ativar" : "Desativar"
+  }</button>
+  `;
 }
 
 testemunhoView();
