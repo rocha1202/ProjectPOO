0779aaef4b2d8c5b2daecb0ff9fd58a5124a1578
rocha1202 <40220247@esmad.ipp.pt>
2024-06-13 19:21:57 +0100

    verificação se o login está feito

---
+++ js/views/logged.js
@@ -0,0 +1,12 @@
+// logged.js
+import * as User from "../models/UsersModel.js";
+
+// Verifica se há um usuário autenticado
+if (!User.isLogged()) {
+    // Se não estiver autenticado, redireciona para outra página
+    window.location.href = "../login.html"; // Substitua com a página desejada
+} else {
+    // Se estiver autenticado, continua o código
+    User.init();
+    let userLogged = User.getUserLogged();
+}
