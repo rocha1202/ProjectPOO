e9fbc524850950506d607eb59295d348a0582f34
rocha1202 <40220247@esmad.ipp.pt>
2024-06-17 19:44:42 +0100

    correção do avatar no ranking

--- js/ranking.js
+++ js/ranking.js
@@ -25,7 +25,6 @@ function exibirRanking() {
     rankingContainer.innerHTML = rankingHTML;
 
     let currentUser = JSON.parse(sessionStorage.getItem('loggedUser')); // Convertendo para objeto JavaScript
-    console.log(currentUser);
 
     if (currentUser) {
         let userIndex = users.findIndex(user => user.nome === currentUser.nome);
@@ -40,3 +39,13 @@ function exibirRanking() {
 
 exibirRanking();
 
+// Carrega os dados do usuário da sessionStorage e exibe no perfil
+var currentUser = JSON.parse(sessionStorage.getItem("loggedUser"));
+if (currentUser) {
+    document.getElementById("pontos").textContent = currentUser.pontos;
+    document.getElementById("avatarImage").src = "/img/avatares/" + currentUser.avatar;
+
+} else {
+    // Se não houver dados de usuário na sessionStorage, redireciona para a página de login
+    window.location.href = "./login.html";
+}
